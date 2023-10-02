d12c06c9d8518a6da2251dc636d5c82c91e5b747
kprze <kacperej@gmail.com>
2023-10-02 14:44:25 +0100

    Add getSliderValue function and modify drawSlider

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-let rowValue = 64;
-let colValue = 64;
+let rowValue = 8;
+let colValue = 8;
+
+let resolution = {
+    1:8,
+    2:16,
+    3:32,
+    4:64,
+    5:128,
+    6:256
+};
+
+const gridContainer = document.querySelector('.gridContainer');
+const sliderContainer = document.querySelector('.sliderContainer');
+
 
 drawGrid(rowValue,colValue);
 draw();
+drawSlider();
+
+let sliderValue = getSliderValue();
+
 
 function drawGrid(rowValue, colValue){
-    // const grid = document.body.appendChild(document.createElement('div'));
-    // grid.className = 'grid';
-    const grid = document.querySelector('.grid')
+    const grid = gridContainer.appendChild(document.createElement('div'));
+    grid.className = 'grid';
 
     for(let i = 0; i < rowValue; i++){
         const row = grid.appendChild(document.createElement('div'));
@@ -21,6 +37,16 @@ function drawGrid(rowValue, colValue){
     };
 };
 
+function drawSlider(){                       
+    const slider = sliderContainer.appendChild(document.createElement('input'));
+    slider.className = 'slider';
+    slider.type = 'range';
+    slider.setAttribute('min','1');
+    slider.setAttribute('max','6');
+    slider.setAttribute('value','4')
+    slider.addEventListener('input', getSliderValue);
+}
+
 function draw(){
     const column = document.querySelectorAll('.column');
     column.forEach((column => {
@@ -29,4 +55,11 @@ function draw(){
             column.style.cssText = 'background: red';
         });
     }));
+};
+
+function getSliderValue(){
+    const slider = document.querySelector('.slider');
+    let resolutionValue = resolution[slider.value];
+    console.log(parseInt(resolutionValue))
+    return parseInt(resolutionValue);
 };
